8faf99534ed8ac331beca4c0ad92940e853b09e0
Sahasra <kesarapusahasra@gmail>
2025-06-05 21:31:33 +0530

    Fix:updated port binding for Render

--- backend/server.js
+++ backend/server.js
@@ -56,6 +56,7 @@ app.get("/api/events/:qrId", async (req, res) => {
 });
 
 // Start server
-app.listen(5000, "0.0.0.0", () => {
-  console.log("🚀 Server is running on http://0.0.0.0:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
